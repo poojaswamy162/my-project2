16e33086eb10a80716303e8eea7087dc4f495943
poojaswamy162 <85693366+poojaswamy162@users.noreply.github.com>
2021-06-12 18:55:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+
+import { element } from 'prop-types';
+import React from 'react';
+import reactDom from 'react-dom';
+class  App extends React.Component{
+render (){
+
+
+return (
+
+<div>
+<img src="/images/xyz.jpeg"  alt="" />
+</div>
+
+
+
+);
+
+   }
+}
+const element = <div ></div>
+ReactDOM.render(element
+   ,document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
